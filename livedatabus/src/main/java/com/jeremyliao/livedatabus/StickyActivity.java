9818266f39acf5d84bd8e9a2c5735218c290c508
guoqj <qingjun@mamahao.com>
2018-11-15 15:57:54 +0800

    测试提交

--- livedatabus/src/main/java/com/jeremyliao/livedatabus/StickyActivity.java
+++ livedatabus/src/main/java/com/jeremyliao/livedatabus/StickyActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.jeremyliao.livedatabus.databinding.ActivityStickyDemoBinding;
@@ -54,6 +53,7 @@ public class StickyActivity extends AppCompatActivity {
             }
         });
         getLifecycle().addObserver(new MyLifecycleObserver());
+        //test
     }
 
     @Override
